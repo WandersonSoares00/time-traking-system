4793f8a28f9814472c0d6a8ad9c64dea84c14bab
Wanderson <wandersonsoared831@gmail.com>
2023-07-24 16:54:53 -0300

    Add paths to new directories

--- src/config/config.php
+++ src/config/config.php
@@ -5,10 +5,17 @@ date_default_timezone_set("America/Fortaleza");
 // Folders
 define("MODEL_PATH",      realpath(dirname(__FILE__) . "/../models"));
 define("VIEW_PATH",       realpath(dirname(__FILE__) . "/../views"));
+define("ASSETS_PATH",     realpath(dirname(__FILE__) . "/../assets"));
 define("CONTROLLER_PATH", realpath(dirname(__FILE__) . "/../controllers"));
 define("TEMPLATE_PATH",   realpath(dirname(__FILE__) . "/../views/template"));
 define("EXCEPTION_PATH",  realpath(dirname(__FILE__) . "/../exceptions"));
 
 // Files
-require_once(realpath(dirname(__FILE__) . "/Database.php"));
+require_once(realpath(dirname(__FILE__) . '/Database.php'));
+include_once(realpath(dirname(__FILE__) . '/loader.php'));
+include_once(realpath(MODEL_PATH        . '/Model.php'));
+include_once(realpath(EXCEPTION_PATH    . '/AppException.php'));
 
+// includes
+//echo realpath(MODEL_PATH . "/Login.php");
+//require_once (realpath(MODEL_PATH . "/Login.php"));
